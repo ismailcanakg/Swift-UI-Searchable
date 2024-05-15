0caccd0c41232bfe240860a57f1708b43924ed7b
İsmail Can Akgün <ismailcanakg1@gmail.com>
2024-05-15 16:42:42 +0300

    Initial Commit

---
+++ SwiftUI-Searchable/SwiftUI_SearchableApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SearchableApp.swift
+//  SwiftUI-Searchable
+//
+//  Created by İsmail Can Akgün on 15.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SearchableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
